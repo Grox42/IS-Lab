8b0e63fdcd2f8a6fde95706a331df93ce0df085f
Grox42 <102907842+Grox42@users.noreply.github.com>
2022-06-06 18:19:10 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using CreditNamespace;
+
+public class Program
+{
+    public static Credit[] Filling(int size)
+    {
+        Credit[] dataBase = new Credit[size];
+        for (int i = 0; i < size; i++)
+            dataBase[i] = new Credit();
+        
+        return dataBase;
+    }
+
+    public static void Main()
+    {
+        Credit request = new Credit("req");
+        request.PrintAll();
+        Credit[] result = request.Search(Filling(50000));
+
+        foreach (var elem in result)
+            elem.PrintAll();
+    }
+}
